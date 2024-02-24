baeeaf0e7531acafaae3cedc43cbf87800ab17a9
KateCory <katecorywright@hotmail.com>
2024-02-24 15:30:52 +0000

    task 8 complete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { getTopics } = require('./controllers/topics-controllers');
 const { getEndpoints } = require('./controllers/endpoints-controller');
-const { getArticleById } = require('./controllers/articles-controller');
+const { getArticleById, patchArticleById } = require('./controllers/articles-controller');
 const { getArticles } = require('./controllers/array-of-articles-controller');
 const { getCommentsById, postCommentsById } = require('./controllers/comments-by-id-controller');
 const app = express();
@@ -15,6 +15,7 @@ app.get('/api', getEndpoints);
 app.get('/api/articles', getArticles);
 
 app.get('/api/articles/:article_id', getArticleById);
+app.patch('/api/articles/:article_id', patchArticleById);
 
 app.get('/api/articles/:article_id/comments', getCommentsById);
 app.post('/api/articles/:article_id/comments', postCommentsById);
--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -1,4 +1,4 @@
-const { selectArticleById } = require("../models/articles-model")
+const { selectArticleById, amendArticleById } = require("../models/articles-model")
 
 exports.getArticleById = (request, response, next) => {
     const { article_id } = request.params;
@@ -10,3 +10,16 @@ exports.getArticleById = (request, response, next) => {
         next(err)
     })
 };
+
+exports.patchArticleById = (request, response, next) => {
+    const { article_id } = request.params;
+    const { inc_votes } = request.body;
+    amendArticleById( {article_id, inc_votes } )
+    .then((article) => {
+        response.status(201).send({ article });
+    })
+    .catch((err) => {
+        next(err)
+    })
+};
+
